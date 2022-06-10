b0d215e12599a054f5c1a9e0b6c5e25849c5f4c0
Samuel Bagattin <42091952+SamuelBagattin@users.noreply.github.com>
2022-06-10 18:23:03 +0200

    Streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,6 @@
 import streamlit
 import pandas as pd
 streamlit.title('Streamlit App')
-jobs = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
-streamlit.dataframe(jobs)
+jobs = pd.read_json('https://jobs.samuelbagattin.com/index.json')
+companies = jobs["Companies"]["Companies"]
+streamlit.dataframe(companies)
