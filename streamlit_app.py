af4cc29ee4d4136a8cf4340679c218410a87d49b
Samuel Bagattin <42091952+SamuelBagattin@users.noreply.github.com>
2022-06-10 18:04:10 +0200

    Streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit
 import pandas as pd
 streamlit.title('Streamlit App')
-jobs = pd.read_json('jobs.samuelbagattin.com/index.json')
+jobs = pd.read_json('https://jobs.samuelbagattin.com/index.json')
 streamlit.dataframe(jobs)
