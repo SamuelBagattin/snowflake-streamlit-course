ae169a01197e103bc6a7fc2217a00e3e5c24069c
Samuel Bagattin <42091952+SamuelBagattin@users.noreply.github.com>
2022-06-10 22:53:11 +0200

    Streamlit

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,15 @@
 import streamlit
 import pandas as pd
+import snowflake
 streamlit.title('Streamlit App')
-jobs = pd.read_json('https://jobs.samuelbagattin.com/index.json')
-companies = jobs["Companies"]["Companies"]
-streamlit.dataframe(companies)
+my_cnx = snowflake.connector.connect(**streamlit.secrets["snowflake"])
+
+my_cur = my_cnx.cursor()
+
+my_cur.execute("SELECT CURRENT_USER(), CURRENT_ACCOUNT(), CURRENT_REGION()")
+
+my_data_row = my_cur.fetchone()
+
+streamlit.text("Hello from Snowflake:")
+
+streamlit.text(my_data_row)
